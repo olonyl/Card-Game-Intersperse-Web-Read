e6b5f6807d3cfca5b9e0b31e6dbae01fc621a149
olonyl <olony.rocha@contollo.net>
2021-09-30 21:58:19 -0500

    Add project files.

---
+++ ConsoleApp2/CardGame.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class CardGame
+    {
+
+        public static List<string> cards = new List<string>{
+    "ace",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "jack",
+    "queen",
+    "king"
+  };
+        public static string ArrayChallenge(string[] strArr)
+        {
+            var totalCards = 0;
+            List<int> aces = new List<int>();
+            //List<int> faces = new List<int>();
+            var maxCard = 0;
+            //Split cards based on their relevance
+            foreach (var card in strArr)
+            {
+                var cardValue = cards.IndexOf(card) + 1;
+                //var cardGameValue = cardValue >= 11? 10:cardValue; 
+                if (cardValue == 1)
+                    aces.Add(1);
+                else if (cardValue >= 11)
+                    totalCards += 10;
+                else totalCards += cardValue;
+                //Capture the highest card
+                if (cardValue > maxCard) maxCard = cardValue;
+            }
+
+            //Return the message in case the total of common cards reaches out 21
+            var strMaxCard = cards[maxCard - 1];
+            if (totalCards > 21) return $"above {strMaxCard}";
+            foreach (var card in aces)
+            {
+                if (totalCards + 11 > 21) totalCards += 1;
+                else totalCards += 11;
+                if (totalCards > 21) return $"above {strMaxCard}";
+            }
+            if (totalCards < 21) return $"below {strMaxCard}";
+            else return "blackjack ace";
+
+        }
+
+        public static void Call()
+        {
+            // keep this function call here
+            Console.WriteLine(ArrayChallenge(new string[] {"ace","queen" }));
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest request = WebRequest.Create("https://coderbyte.com/api/challenges/json/age-counting");
+            WebResponse response = request.GetResponse();
+            var strEqual = "age=";
+            var indexOfAge = 0;
+            var indexOfEqual = 0;
+            var indexOfComma = 0;
+            var numberGTE = 0;
+            var challengeToken = "s6ofjtk5pa3";
+            var result = string.Empty;
+
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(dataStream);
+                string strResponse = reader.ReadToEnd();
+                //The idea is going trimming the string response until nothing left after read the value I need to extract
+                while (strResponse.Length > 10)
+                {
+                    indexOfAge = strResponse.IndexOf(strEqual);
+                    strResponse = strResponse.Remove(0, indexOfAge);
+                    indexOfAge = strResponse.IndexOf(strEqual);
+                    indexOfEqual = indexOfAge + strEqual.Length;
+                    indexOfComma = strResponse.IndexOf(",");
+                    if (indexOfComma - indexOfEqual >= 0)
+                    {
+                        var age = int.Parse(strResponse.Substring(indexOfEqual, indexOfComma - indexOfEqual));
+                        if (age >= 50) numberGTE++;
+                        if (indexOfComma > 0)
+                            strResponse = strResponse.Remove(0, indexOfComma);
+                    }
+                }
+                var resultLength = numberGTE.ToString().Length - 1;
+                for (var i = 0; i <= resultLength; i++)
+                {
+                    result += numberGTE.ToString()[i];
+                    if (challengeToken != string.Empty)
+                    {
+                        if (i < resultLength)
+                        {
+                            result += challengeToken[0];
+                            challengeToken = challengeToken.Remove(0, 1);
+                        }
+                        else
+                        {
+                            result += challengeToken;
+                            challengeToken = string.Empty;
+                        }
+
+                    }
+                }
+                Console.WriteLine(result);
+            }
+            response.Close();
+        }
+    }
+}
+
